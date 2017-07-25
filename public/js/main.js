bfd5ea95c75a6827cb733cb06c7cef14f413493e
Anil Shrestha <jesnilshrestha04@gmail.com>
2017-07-24 17:23:03 -0700

    display_all_companies

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/* A get request to get all the data */
+var xhr = new XMLHttpRequest();
+xhr.onload = function(){
+    if(xhr.status === 200){
+        responseObject = JSON.parse(xhr.responseText);
+
+        var mainContent = '';
+        for (var i =0; i < responseObject.results.length; i ++){
+            mainContent += '<tr> <td>';
+            mainContent += '<img src="'+ responseObject.results[i].avatarUrl + '" ';
+            mainContent += 'alt="' + responseObject.results[i].name + '" />';
+            mainContent += '</td><td>';
+            mainContent += '<p>' +responseObject.results[i].name + ' </p>'; 
+            mainContent += '</td></tr>'; 
+        }
+        document.getElementById('company').innerHTML = mainContent;
+    }
+};
+xhr.open('GET', 'http://localhost:3000/api/companies')
+xhr.send(null);
